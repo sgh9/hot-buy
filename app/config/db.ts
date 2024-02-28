5c6c473400f1129dee70573d959c88cf5f79efca
Sangamesh <hiremath280997@gmail.com>
2024-02-29 01:40:52 +0530

    changes for env

--- app/config/db.ts
+++ app/config/db.ts
@@ -3,7 +3,7 @@ import { connect } from 'mongoose';
 export const dbConnect = async()=> {
     console.log({url: process.env.MONGODB_URI})
     try {
-       const db =  await connect(process.env.MONGODB_URI!);
+       const db =  await connect(process.env.MONGODB_URI || 'mongodb+srv://sangamesh:abc%40123@realmcluster.jyvrl.mongodb.net/hotBuy');
        console.log({port: db.connection.port})
        
     } catch (error: any) {
